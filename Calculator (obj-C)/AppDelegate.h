95a861308d70ee24566696cf3362dc3bb07fd04b
Admin <admin@Mac-Admin.local>
2015-08-21 16:00:06 +0300

    Initial Commit

---
+++ Calculator (obj-C)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator (obj-C)
+//
+//  Created by Admin on 21.08.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator (obj-C)Tests/Calculator__obj_C_Tests.m
@@ -0,0 +1,40 @@
+//
+//  Calculator__obj_C_Tests.m
+//  Calculator (obj-C)Tests
+//
+//  Created by Admin on 21.08.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Calculator__obj_C_Tests : XCTestCase
+
+@end
+
+@implementation Calculator__obj_C_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
